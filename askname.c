5b16ee4a3ebca525b767c5faa001ffcbc2455a63
Add Mayers <mayersuru@gmail.com>
2021-01-13 17:39:54 +0300

    okaaaay
    # Conflicts:
    #	askname.c
    #	main.c

--- askname.c
+++ askname.c
@@ -3,7 +3,11 @@
 
 void askname(char *first, char *last)
 {
+<<<<<<< HEAD
     printf("Enter your first name pls: My name is Nastya");
+=======
+    printf("Enter your first name pls: ");
+>>>>>>> 33fe8b4 (okaaaay)
     fgets(first, 255, stdin);
     first[strlen(first)-1] = '\0'; /* remove the newline at the end */
 
--- main.c
+++ main.c
@@ -9,10 +9,14 @@ int main(int argc, char **argv)
     askname(first, last);
 
 <<<<<<< HEAD
+<<<<<<< HEAD
     printf("Hello! %s %s!\n", first, last);
 =======
     printf("hello earthlings, I made changes , %s %s!\n", first, last);
 >>>>>>> d9b4767 (my change!)
+=======
+    printf("Hello! %s %s!\n", first, last);
+>>>>>>> 33fe8b4 (okaaaay)
     return 0;
 }
 
